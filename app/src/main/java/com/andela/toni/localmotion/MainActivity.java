d9bde714633840aa515583214d2ac5e098d9d50f
andela-tsolarin <toni.edward@outlook.com>
2015-10-27 18:00:32 +0100

    Add visual feedback to primary buttons when they are interacted with

--- app/src/main/java/com/andela/toni/localmotion/MainActivity.java
+++ app/src/main/java/com/andela/toni/localmotion/MainActivity.java
@@ -2,20 +2,53 @@ package com.andela.toni.localmotion;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button btnTrack;
+    private Button btnHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTrack = (Button) findViewById(R.id.btnTrack);
+        btnHistory = (Button) findViewById(R.id.btnHistory);
+
+        btnTrack.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                showButtonVisualFeedback(btnTrack, event);
+                return false;
+            }
+        });
+
+        btnHistory.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                showButtonVisualFeedback(btnHistory, event);
+                return false;
+            }
+        });
+    }
+
+    private void showButtonVisualFeedback(Button btn, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                btn.setBackgroundResource(R.drawable.secondary_button);
+                break;
+            case MotionEvent.ACTION_UP:
+                btn.setBackgroundResource(R.drawable.primary_button);
+                break;
+            default:
+                btn.setBackgroundResource(R.drawable.secondary_button);
+        }
     }
 
     @Override
