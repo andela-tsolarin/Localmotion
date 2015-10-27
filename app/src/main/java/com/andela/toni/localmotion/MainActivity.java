ce2f5e0854688abadd4f751388cec396e4907316
andela-tsolarin <toni.edward@outlook.com>
2015-10-27 17:16:01 +0100

    Add scaled image asset to drawables folder. Add image to main activity

--- app/src/main/java/com/andela/toni/localmotion/MainActivity.java
+++ app/src/main/java/com/andela/toni/localmotion/MainActivity.java
@@ -1,5 +1,6 @@
 package com.andela.toni.localmotion;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,14 +10,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
     @Override
