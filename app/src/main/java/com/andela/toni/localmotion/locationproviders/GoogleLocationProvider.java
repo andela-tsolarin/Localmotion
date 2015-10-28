89c1a890996f1c53ebf5fd770bb048ab892fcc39
andela-tsolarin <toni.edward@outlook.com>
2015-10-28 12:27:44 +0100

    Add location changed event handler to GoogleLocationProvider class

--- app/src/main/java/com/andela/toni/localmotion/locationproviders/GoogleLocationProvider.java
+++ app/src/main/java/com/andela/toni/localmotion/locationproviders/GoogleLocationProvider.java
@@ -1,19 +1,24 @@
 package com.andela.toni.localmotion.locationproviders;
 
 import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
 
 /**
  * Created by tonie on 10/28/2015.
  */
-public class GoogleLocationProvider implements LocationProvider, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class GoogleLocationProvider implements LocationProvider, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
     private Context context;
 
     public GoogleLocationProvider(Context context){
@@ -28,6 +33,25 @@ public class GoogleLocationProvider implements LocationProvider, GoogleApiClient
     @Override
     public void onConnected(Bundle bundle) {
         Toast.makeText(this.context, "Connected to Google Play Services", Toast.LENGTH_LONG).show();
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            Toast.makeText(this.context, "Latitude:" + mLastLocation.getLatitude()+", Longitude:"+mLastLocation.getLongitude(),Toast.LENGTH_LONG).show();
+        }
+
+        createLocationRequest();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(20000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        startLocationUpdates();
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
     }
 
     @Override
@@ -53,5 +77,10 @@ public class GoogleLocationProvider implements LocationProvider, GoogleApiClient
             mGoogleApiClient.disconnect();
             Toast.makeText(this.context, "Disconnected from Google Play Services", Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(this.context, "Latitude:" + location.getLatitude()+", Longitude:"+location.getLongitude(), Toast.LENGTH_SHORT).show();
     }
 }
